9840c4fe98123b81919dc23add213c3616577d85
RicardoHernandez07 <63927411+RicardoHernandez07@users.noreply.github.com>
2020-04-20 14:12:33 -0700

    Add files via upload

---
+++ Lista doblemente ligada/Lista doblemente ligada/Lista.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_doblemente_ligada
+{
+    class Lista
+    {
+        private Nodo primero = new Nodo();
+        private Nodo atras = new Nodo();
+
+        public Lista()
+        {
+            primero = null;
+            atras = null;
+        }
+
+        public void insertarnodo()
+        {
+            Nodo nuevo = new Nodo();
+            Console.Write(" ingrese el numero para la lista doble ");
+            nuevo.Dato = int.Parse(Console.ReadLine());
+            if (primero == null) 
+            {
+                primero = nuevo;
+                primero.Siguiente = null;
+                primero.Atras = null;
+                atras = primero;
+            }
+
+            else
+            {
+                atras.Siguiente = nuevo;
+                nuevo.Siguiente = null;
+                nuevo.Atras = atras;
+                atras = nuevo; 
+            }
+            Console.Write(" El numero fue ingresado con exito ");
+        }
+
+        public void desplegarListaPU()
+        {
+            Nodo actual = new Nodo();
+            actual = primero;
+            while (actual != null)
+            {
+                Console.WriteLine(" " + actual.Dato);
+                actual = actual.Siguiente;
+            }
+        }
+        public void desplegarlistaUP()
+        {
+            Nodo actual = new Nodo();
+            actual = atras;
+            while (actual != null)
+            {
+                Console.WriteLine(" " + actual.Dato);
+                actual = actual.Atras;
+            }
+        }
+    }
+}
---
+++ Lista doblemente ligada/Lista doblemente ligada/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_doblemente_ligada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista l = new Lista();
+            l.insertarnodo();
+            l.insertarnodo();
+            l.insertarnodo();
+            l.insertarnodo();
+            l.insertarnodo();
+            Console.WriteLine("Del primero al Ultimo");
+            l.desplegarlistaUP();
+            Console.WriteLine("Del ultimo al primero");
+            l.desplegarListaPU();
+            Console.ReadLine();
+
+        }
+    }
+}
